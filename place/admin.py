7a9ec264dac6ab5e9a40c43c8249fe0ac9b299d2
samioncuer <sami.oncuer1@hotmail.com>
2020-04-05 05:52:56 +0300

    güncelleme

--- place/admin.py
+++ place/admin.py
@@ -1,10 +1,29 @@
 from django.contrib import admin
 
 # Register your models here.
-from place.models import Category
-class CategoryAdmin(admin.ModelAdmin):
+from place.models import Category, Place, Images
 
-    list_display = ['title', 'status']
+class PlaceImageInline(admin.TabularInline):
+    model = Images
+    extra = 5
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['title', 'status', 'image_tag']
     list_filter = ['status']
+    readonly_fields = ('image_tag',)
+
+class PlaceAdmin(admin.ModelAdmin):
+    list_display = ['title', 'category', 'price', 'address', 'image_tag', 'status']
+    readonly_fields = ('image_tag',)
+    list_filter = ['status', 'category']
+    inlines = [PlaceImageInline]
+
+
+class ImagesAdmin(admin.ModelAdmin):
+    list_display = ['title', 'place', 'image_tag']
+    readonly_fields = ('image_tag',)
 
 admin.site.register(Category,CategoryAdmin)
+admin.site.register(Place, PlaceAdmin)
+admin.site.register(Images, ImagesAdmin)
